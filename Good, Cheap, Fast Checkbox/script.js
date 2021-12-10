246f4dd206d1a116a3bebc51d1a9980a40729069
Jorge Francisco <Jorge7.9@hotmail.com>
2021-12-10 09:37:46 -0300

    Good, Cheap, Fast

---
+++ Good, Cheap, Fast Checkbox/script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelectorAll('.toggle');
+const good = document.getElementById('good');
+const cheap = document.getElementById('cheap');
+const fast = document.getElementById('fast');
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickedOn) {
+  if(good.checked && cheap.checked &&fast.checked) {
+    if(good === theClickedOn) {
+      fast.checked = false;
+    }
+
+    if(cheap === theClickedOn) {
+      good.checked = false;
+    }
+
+    if(fast === theClickedOn) {
+      cheap.checked = false;
+    }
+  }
+}
